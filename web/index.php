90dac920cb636dd14d0e5cf03dc60cd7b3e4589d
Sam Cohen <samc1213@gmail.com>
2015-11-21 19:46:58 -0600

    hellow orld gone

--- web/index.php
+++ web/index.php
@@ -6,8 +6,6 @@ Braintree_Configuration::merchantId('vy8vxfry49ydvw6x');
 Braintree_Configuration::publicKey('ghhmmmsctxvbkz7m');
 Braintree_Configuration::privateKey('72d92b61ab66470d32e9b2dda5d2caf2');
 
-echo 'hello world';
-
 echo($clientToken = Braintree_ClientToken::generate());
 
 ?>
