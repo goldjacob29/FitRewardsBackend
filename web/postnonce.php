49efa62f9b677896d1784d99c3a89012b49c8e46
Sam Cohen <samc1213@gmail.com>
2015-11-21 22:37:07 -0600

    actually making payment

--- web/postnonce.php
+++ web/postnonce.php
@@ -1,10 +1,18 @@
 <?php
 	require('../vendor/autoload.php');
+	Braintree_Configuration::environment('sandbox');
+	Braintree_Configuration::merchantId('vy8vxfry49ydvw6x');
+	Braintree_Configuration::publicKey('ghhmmmsctxvbkz7m');
+	Braintree_Configuration::privateKey('72d92b61ab66470d32e9b2dda5d2caf2');
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Credentials: true");
 	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 	header("Access-Control-Allow-Headers: DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
 	$nonce = $_POST['nonce'];
-	echo($nonce);
-	echo("EXXTRA!");
+
+	$result = Braintree_Transaction::sale([
+	  'amount' => '100.00',
+	  'paymentMethodNonce' => $nonce
+	]);
+	echo($result);
 ?>
