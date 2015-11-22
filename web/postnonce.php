571e3c09bf1769b8f724f0d409dc12195f1b676c
Sam Cohen <samc1213@gmail.com>
2015-11-21 21:57:56 -0600

    goddamn

--- web/postnonce.php
+++ web/postnonce.php
@@ -2,6 +2,7 @@
 	require('../vendor/autoload.php');
 	header("Access-Control-Allow-Origin: *");
 	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+	header("Access-Control-Allow-Headers: X-Requested-With");
 	$nonce = $_POST['nonce'];
 	echo($nonce);
 ?>
