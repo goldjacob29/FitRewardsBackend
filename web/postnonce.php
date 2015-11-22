7ffc31dff850df49c5d1047f4d5e1805065e3a6c
Sam Cohen <samc1213@gmail.com>
2015-11-21 21:51:05 -0600

    more cors shit

--- web/postnonce.php
+++ web/postnonce.php
@@ -1,7 +1,7 @@
 <?php
 	require('../vendor/autoload.php');
 	header("Access-Control-Allow-Origin: *");
+	header('Access-Control-Allow-Methods: GET, POST');
 	$nonce = $_POST['nonce'];
-	echo("noncey");
 	echo($nonce);
 ?>
