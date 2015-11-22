3a6010b4c708df2cf1899d86ad90fe30606be96d
Sam Cohen <samc1213@gmail.com>
2015-11-21 18:57:33 -0600

    adding btree

--- web/index.php
+++ web/index.php
@@ -1,6 +1,11 @@
 <?php
 
 require('../vendor/autoload.php');
+Braintree_Configuration::environment('sandbox');
+Braintree_Configuration::merchantId('use_your_merchant_id');
+Braintree_Configuration::publicKey('use_your_public_key');
+Braintree_Configuration::privateKey('use_your_private_key');
 
-echo 'hey world!';
+echo 'hello world';
+
 ?>
