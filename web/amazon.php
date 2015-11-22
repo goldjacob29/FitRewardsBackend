3d5a4c3560eeab419ab535d79763c21a17ad2914
Sam Cohen <samc1213@gmail.com>
2015-11-21 21:41:42 -0600

    added postnonce

---
+++ web/amazon.php
@@ -0,0 +1,7 @@
+<?php
+
+require('../vendor/autoload.php');
+header("Access-Control-Allow-Origin: *");
+
+echo 'hihi';
+?>
---
+++ web/postnonce.php
@@ -0,0 +1,8 @@
+<?php
+	require('../vendor/autoload.php');
+	header("Access-Control-Allow-Origin: *");
+	$nonce = 'boobies';
+	$nonce = $_POST['nonce'];
+	echo "noncey"
+	echo $nonce;
+?>
